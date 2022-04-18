68605c17a0cd9bfd14e54ae6577dbde3df9a32c5
Florian2501 <florischierz@gmail.com>
2022-04-18 19:50:58 +0200

    Implementierung Start

---
+++ CProgrammierung/Implementation/bloomfilter.c
@@ -0,0 +1,10 @@
+#include "murmur.h"
+#include <stdio.h>
+
+int main(void){
+
+    printf("%d", sizeof(unsigned int));
+
+    //Array erstellem mit bestimmter größe und entsprechend vielen hash fkt
+    return 0;
+}
