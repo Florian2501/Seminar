abbe3d27d9214d162edc5dd826cd44808364ffca
Florian2501 <florischierz@gmail.com>
2022-04-28 18:51:07 +0200

    BF funktionsfähig & FF implementiert im Vergleich

--- CProgrammierung/Implementation/bloomfilter.c
+++ CProgrammierung/Implementation/bloomfilter.c
@@ -4,18 +4,39 @@
 #include "murmur.h"
 
 //BF steht fuer Bloom-Filter
-//False Positive Propability (FPP) --> Fehlerrate mit der ausgibt, dass Element entahlten, obwohl es nicht enthalten ist
+//False Positive Probability (FPP) --> Fehlerrate mit der ausgibt,
+//dass Element entahlten, obwohl es nicht enthalten ist
+#define FPP 0.001
+
 //Benoetigte Speichergroesse m in Bit
+
 //Anzahl zu speichernder Elemente n
+#define n 50
+
 //Optimale Anzahl Hash-Funktionen k
+
 //Formeln:
 //m = - n*ln(FPP)/(ln(2))^2
 //k = m/n * ln(2)
 
-#define FFP 0.05
-#define n 100
+#define bereiche 18
+
+#define filename "numbers.txt"
 
 //#define DEBUG
+
+//Farbwerte von https://stackoverflow.com/questions/3585846/color-text-in-terminal-applications-in-unix
+//aus dem Beitrag von David Guyon
+//aufgerufen am 28.04.2022 um 18:45 Uhr
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
 
 //Gibt den in bloom übergebenen BF binaer aus
 // m in Bit
@@ -38,7 +59,7 @@ void printBF(char* bloom, int m)
 int berechneM()
 {
     //ceil rundet dabei das Ergebnis auf die naechste Ganzzahl auf
-    return (int)(ceil( (-1)*n*log(FFP)/(log(2)*log(2)) ));
+    return (int)(ceil( (-1)*n*log(FPP) / (log(2)*log(2)) ));
 }
 
 //Die optimale Anzahl k Hash-Funktionen wird mittels der bekannten Formel berechnet
@@ -49,9 +70,10 @@ int berechneK(int m)
     return (int)(round( (double)m/n * log(2) ));
 }
 
-//Das in element uebergebene Element wird in den in bloom übergebenen Bloom-Filter eingefügt mittels k Hash-Funktionen
+//Das in element uebergebene Element wird in den in bloom übergebenen Bloom-Filter 
+//eingefügt mittels k Hash-Funktionen
 //m ist die Länge des BF in Bit
-int einfuegen(char* bloom, char* element, int k, int m)
+void einfuegen(char* bloom, char* element, int k, int m)
 {    
     //Pro Element entsprechend der Anzahlen der Hash Funktionen Bits im BF setzen
     for(int i = 0; i < k; i++)
@@ -68,15 +90,177 @@ int einfuegen(char* bloom, char* element, int k, int m)
         
         #ifdef DEBUG
         //Ausgabe der Hash Werte und des ganzen BF zur Verdeutlichung
-        printf("Hash %d von %s: %d (Byte: %d, Bit: %d)\n", i+1, element, position, char_position, bit_position);
+        printf("Bloom: Hash %d von %s: %d (Byte: %d, Bit: %d)\n", i+1, element, position, char_position, bit_position);
         printBF(bloom, m);
         #endif
     }
 }
 
+//Es wird geprueft, ob das in element uebergebene Element
+//im in bloom übergebenen Bloom-Filter vorhanden ist
+//m ist die Länge des BF in Bit
+//k ist die Anzahl Hash Funktionen
+//Gibt 0 aus wenn nicht enthalten
+//Gibt 1 aus wenn enthalten
+int pruefen(char* bloom, char* element, int k, int m)
+{
+    #ifdef DEBUG
+    printf("\nWort: %s\n", element);
+    #endif
+
+    //Pro Element entsprechend der Anzahlen der Hash Funktionen Bits im BF pruefen
+    for(int i = 0; i < k; i++)
+    {
+        //Murmur2 Hash des zu pruefenden Elements wird berechnet und auf die maximal moegliche 
+        //Laenge des BF heruntergebrochen
+        unsigned int position = murmur2(element, sizeof(element), i) % m;
+
+        //Umrechnung der Position des Elements in Byte und Einzelbits im dann referenzierten Byte
+        unsigned int char_position = position / 8;
+        unsigned int bit_position = position % 8;
+
+        #ifdef DEBUG
+        printf("%d: Byte: %d, Bit: %d\n", i, char_position, bit_position);
+        #endif
+
+        //Bit an der Stelle des Elements testen
+        if(!(bloom[char_position] & (1<<bit_position)))
+        {
+            #ifdef DEBUG
+            printf("%s ist sicher nicht im BF enthalten.\n", element);
+            #endif
+
+            //Wenn das Bit an der Stelle 0 ist kann das Element nicht enthalten sein --> Abbruch
+            return 0;
+        }
+        
+    }
+
+    #ifdef DEBUG
+    printf("Es wird vermutet, dass %s im BF enthalten ist.\n", element);
+    #endif
+
+    //Wird die Schleife vollstaendig durchlaufen, muessen immer Einsen an den Stellen gestanden
+    //haben --> Vermutung, dass enthalten
+    return 1;
+}
+
+//TODO kommentieren einfuegenFF und pruefenFF
+//TODO Vergleich zu BF wenn einfach nur um bestimmten Platz größer wäre
+
+void einfuegenFF(char* floom, char* element, int k, int m, char* speicher)
+{    
+    int speicher_position = 0;
+    //Pro Element entsprechend der Anzahlen der Hash Funktionen Bits im BF setzen
+    for(int i = 0; i < k; i++)
+    {
+        //Murmur2 Hash des einzufuegenden Elements wird berechnet und auf die maximal moegliche Laenge des BF heruntergebrochen
+        unsigned int position = murmur2(element, sizeof(element), i) % m;
+
+        int j=1;
+        while(position > j*m/bereiche){
+            j++;
+        }
+
+        speicher_position = speicher_position | (1<<(j-1));
+
+        //Umrechnung der Position des Elements in Byte und Einzelbits im dann referenzierten Byte
+        unsigned int char_position = position / 8;
+        unsigned int bit_position = position % 8;
+
+        //Bit an der Stelle des Elements setzen
+        floom[char_position] |= (1<<bit_position);
+        
+        #ifdef DEBUG
+        //Ausgabe der Hash Werte und des ganzen BF zur Verdeutlichung
+        printf("Floom: Hash %d von %s: %d (Byte: %d, Bit: %d)\n", i+1, element, position, char_position, bit_position);
+        printf("Speicherposition liegt in Bereich %d von %d.\n", j, bereiche);
+        printf("Bereichsgroesse: %d Byte bzw. %d Bit.\n", m/bereiche/8, m/bereiche);
+        printBF(floom, m);
+        #endif
+    }
+
+    unsigned int char_position = speicher_position / 8;
+    unsigned int bit_position = speicher_position % 8;
+    speicher[char_position] |= (1 << bit_position);
+
+    #ifdef DEBUG
+    //Ausgabe der Hash Werte und des ganzen BF zur Verdeutlichung
+    printf("%s wurde an der Bitstelle %d in den extra Speicher eingefuegt.\n", element, speicher_position);
+    printBF(speicher, pow(2, (bereiche-3.0)) * 8);
+    #endif
+}
+
+int pruefenFF(char* floom, char* element, int k, int m, char* speicher)
+{
+    #ifdef DEBUG
+    printf("\nWort: %s\n", element);
+    #endif
+    int speicher_position = 0;
+
+    //Pro Element entsprechend der Anzahlen der Hash Funktionen Bits im BF pruefen
+    for(int i = 0; i < k; i++)
+    {
+        //Murmur2 Hash des zu pruefenden Elements wird berechnet und auf die maximal moegliche 
+        //Laenge des BF heruntergebrochen
+        unsigned int position = murmur2(element, sizeof(element), i) % m;
+
+        int j=1;
+        while(position > j*m/bereiche){
+            j++;
+        }
+
+        speicher_position = speicher_position | (1<<(j-1));
+
+        //Umrechnung der Position des Elements in Byte und Einzelbits im dann referenzierten Byte
+        unsigned int char_position = position / 8;
+        unsigned int bit_position = position % 8;
+
+        #ifdef DEBUG
+        printf("%d: Byte: %d, Bit: %d\n", i, char_position, bit_position);
+        #endif
+
+        //Bit an der Stelle des Elements testen
+        if(!(floom[char_position] & (1<<bit_position)))
+        {
+            #ifdef DEBUG
+            printf("%s ist sicher nicht im FF enthalten.\n", element);
+            #endif
+
+            //Wenn das Bit an der Stelle 0 ist kann das Element nicht enthalten sein --> Abbruch
+            return 0;
+        }
+        
+    }
+
+    
+
+    unsigned int char_position = speicher_position / 8;
+    unsigned int bit_position = speicher_position % 8;
+
+    if(!(speicher[char_position] & (1 << bit_position)))
+    {
+        #ifdef DEBUG
+        printf("%s ist sicher nicht im FF enthalten.\n", element);
+        printf("Feststellung dank extra Speicher!\n");
+        printBF(speicher, pow(2, (bereiche-3.0)) * 8 );
+        #endif
+        return 0;
+    }
+
+
+    #ifdef DEBUG
+    printf("Es wird vermutet, dass %s enthalten ist.\n", element);
+    #endif
+    //Wird die Schleife vollstaendig durchlaufen, muessen immer Einsen an den Stellen gestanden
+    //haben und wenn im Speicher an der Bereichsstelle auch eine Eins war --> Vermutung, dass enthalten
+    return 1;
+}
+
 int main(void)
 {
-    //Aus den gegebenen Groessen (Anzahl einzufuegende Elemente und maximale FFP) werden m und k berechnet
+    //Aus den gegebenen Groessen (Anzahl einzufuegende Elemente und maximale FPP) 
+    //werden m und k berechnet
     int m = berechneM();
     printf("Der Bloom-Filter wird mit der Groesse m=%d Bit initialisiert...\n", m);
 
@@ -90,7 +274,8 @@ int main(void)
     printf("Der Bloom-Filter wird erstellt mit %d Byte...\n", m_in_byte);
 
     //Bloom-Filter wird mit der berechneten Groesse in Byte erstellt
-    //Die evtl. maximal 7 "zu vielen" Bits werden mitverwendet, da der Platz sowieso nicht anders verwendet werden kann
+    //Die evtl. maximal 7 "zu vielen" Bits werden mitverwendet, da der Platz sowieso 
+    //nicht anders verwendet werden kann
     char* bloom = malloc(m_in_byte * sizeof(char));
 
     //Sicherstellen, dass allo Werte im BF 0 sind
@@ -99,17 +284,43 @@ int main(void)
         bloom[i] = 0;
     }
 
-    printf("Fuellen des Bloom-Filters mit %d englischen Worten...\n", n);
 
-    //Die Liste der verschiedenen englischen Worte ist von https://github.com/dwyl/english-words und ist frei verfuegbar
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Floom Filter
 
-    char* filename = "words4.txt";
+    char* floom =  malloc(m_in_byte * sizeof(char));
+
+    //Sicherstellen, dass allo Werte im FF 0 sind
+    for(int i = 0; i<m_in_byte; i++)
+    {
+        floom[i] = 0;
+    }
+
+    //in Byte
+    int speicher_groesse_in_byte = pow(2, (bereiche-3.0));
+    char* speicher = malloc( speicher_groesse_in_byte * sizeof(char) );
+
+    for(int i = 0; i<speicher_groesse_in_byte; i++)
+    {
+        speicher[i] = 0;
+    }
+
+    printf("\n==============================================================================\n");
+    printf("Zusaetzlicher Speicherbereich mit %d Bits bzw. %d Bytes wurde initialisiert.\n", speicher_groesse_in_byte*8, speicher_groesse_in_byte);
+    printf("Darin werden die Belegungsinformationen der %d Bereiche gespeichert.\n", bereiche);
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    printf("Fuellen des Bloom-Filters mit %d zufaelligen Zahlen...\n", n);
+
     FILE* file = fopen(filename, "r");
 
     //Testen, ob Oeffnen funktioniert hat
     if(file == NULL)
     {
-        printf("Fehler beim oeffnen der Wortdatei %s. Abbruch des Fuellens!\n", filename);
+        printf("Fehler beim oeffnen der Textdatei %s. Abbruch des Fuellens!\n", filename);
         return EXIT_FAILURE;
     }
 
@@ -118,22 +329,110 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        //Wort einlesen
-        fscanf(file, "%s", word);
+        //Wort einlesen und pruefen, ob das Dateiende erreicht wird
+        if(fscanf(file, "%s", word) == EOF)
+        {
+            printf("=============================================================\n");
+            printf("Die Anzahl einzulesender Elemente ueberschreitet die Anzahl ");
+            printf("vorhandener Elemente in der Datei.\n");
+            printf("Abbruch nach %d gelesenen Elementen.\n", i);
+            break;
+        }
 
         //Wort in Bloom-Filter einfuegen
         //Da der uebrige Platz im letzten evtl. angerissenen Byte mitgenutzt werden soll, wird nicht die
         //genaue Anzahl Bit angegeben, die durch die Formel errechnet wurde, sondern die auf volle Byte 
         //aufgerundete Anzahl Bit, da der Platz im evtl. angerissenen Byte sonst verloren ginge
         einfuegen(bloom, word, k, m_in_byte*8);
+
+        einfuegenFF(floom, word, k, m_in_byte*8, speicher);
     }
 
-    printBF(bloom, m_in_byte*8);
+    //Gibt den gesamten BF aus
+    //printBF(bloom, m_in_byte*8);
+
+    //Zaehlvariablen fuer die Evaluation des BF
+    int gesamt = 0;
+    int falschpositivBF = 0;
+
+    int falschpositivFF = 0;
+
+    //Restliche Zahlen durchlaufen und auf Mitgliedschaft im BF testen
+    while(fscanf(file, "%s", word) != EOF) // && gesamt < n)
+    {
+        gesamt++;
+
+        //Da in der Liste keine Zahl doppelt steht, ist die korrekte Antwort immer 
+        //"Nicht enthalten". Darum ist jede Antwort "Enthalten" automatisch falsch
+        if(pruefen(bloom, word, k, m_in_byte*8))
+        {
+            falschpositivBF++;
+        }
+
+        if(pruefenFF(floom, word, k, m_in_byte*8, speicher))
+        {
+            falschpositivFF++;
+        }
+
+        #ifdef DEBUG
+            printf("\nGesamtanzahl: %d, Bislang falsch-positive beim BF: %d\n",  gesamt, falschpositivBF);
+            printf("Gesamtanzahl: %d, Bislang falsch-positive beim FF: %d\n",  gesamt, falschpositivFF);
+
+        #endif
+    }
+
+    //Aus den gemssenen Anzahlen kann eine experimentell ermittelte FPP berechnet werden
+    double fpp_gemessen = (double)falschpositivBF / gesamt;
+
+    printf("\n==============================================================================\n");
+    printf("Vergleich der theoretischen und der gemessenen False Positive Probability:\n");
+    printf("Vorgegebene FPP: %1.5f\n", FPP);
+    printf("Gemessene FPP im BF:   %1.5f\n", fpp_gemessen);
+
+    //Prozentuale Abweichung berechnen
+    double abweichung = ((fpp_gemessen / FPP) - 1) * 100.0;
+    printf("Abweichung in Prozent: ");
+    if(abweichung > 0)
+    {
+        printf(RED);
+    }
+    else if(abweichung < 0)
+    {
+        printf(GRN);
+    }
+    printf("%.2f %%\n" RESET, abweichung);
+
+
+    double fpp_gemessenFF = (double)falschpositivFF / gesamt;
+    
+    printf("\nGemessene FPP im FF:   %1.5f\n", fpp_gemessenFF);
+
+    //Prozentuale Abweichung berechnen
+    double abweichungFF = ((fpp_gemessenFF / FPP) - 1) * 100.0;
+    printf("Abweichung in Prozent: ");
+    if(abweichungFF > 0)
+    {
+        printf(RED);
+    }
+    else if(abweichungFF < 0)
+    {
+        printf(GRN);
+    }
+    printf("%.2f %%\n" RESET, abweichungFF);
+
+    printf("\nVerbeserung durch extra Speicher: ");
+    if(abweichung-abweichungFF > 0) 
+    {
+        printf(GRN);
+    }
+    printf("%.2f %%\n" RESET, abweichung-abweichungFF);
 
     //words.txt schliessen
     fclose(file);
     //Bloom Filter freigeben
     free(bloom);
+    free(floom);
+    free(speicher);
 
     return EXIT_SUCCESS;
 }
--- CProgrammierung/Implementation/murmur.h
+++ CProgrammierung/Implementation/murmur.h
@@ -5,7 +5,9 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdint.h>
 
+/*
 unsigned int murmur2 ( const void * key, int len, unsigned int seed )
 {
 	// 'm' and 'r' are mixing constants generated offline.
@@ -59,7 +61,10 @@ unsigned int murmur2 ( const void * key, int len, unsigned int seed )
 	return h;
 } 
 
-/*unsigned long int  murmur1( const void * key, int len, unsigned int seed )
+*/
+
+/*
+unsigned long int  murmur1( const void * key, int len, unsigned int seed )
 {
 	const unsigned long long int  m = 0xc6a4a7935bd1e995;
 	const int r = 47;
@@ -102,3 +107,118 @@ unsigned int murmur2 ( const void * key, int len, unsigned int seed )
 	return h;
 } 
 */
+
+
+#define _MURMURHASH3_H_
+
+#ifdef __GNUC__
+#define FORCE_INLINE __attribute__((always_inline)) inline
+#else
+#define FORCE_INLINE inline
+#endif
+
+static FORCE_INLINE uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+  return (x << r) | (x >> (32 - r));
+}
+
+static FORCE_INLINE uint64_t rotl64 ( uint64_t x, int8_t r )
+{
+  return (x << r) | (x >> (64 - r));
+}
+
+#define	ROTL32(x,y)	rotl32(x,y)
+#define ROTL64(x,y)	rotl64(x,y)
+
+#define BIG_CONSTANT(x) (x##LLU)
+
+//-----------------------------------------------------------------------------
+// Block read - if your platform needs to do endian-swapping or can only
+// handle aligned reads, do the conversion here
+
+#define getblock(p, i) (p[i])
+
+//-----------------------------------------------------------------------------
+// Finalization mix - force all bits of a hash block to avalanche
+
+static FORCE_INLINE uint32_t fmix32 ( uint32_t h )
+{
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
+//----------
+
+static FORCE_INLINE uint64_t fmix64 ( uint64_t k )
+{
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+  k ^= k >> 33;
+
+  return k;
+}
+
+//-----------------------------------------------------------------------------
+
+
+unsigned int murmur2 ( const void * key, int len,
+                          uint32_t seed)
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+  int i;
+
+  uint32_t h1 = seed;
+
+  uint32_t c1 = 0xcc9e2d51;
+  uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  for(i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+    
+    h1 ^= k1;
+    h1 = ROTL32(h1,13); 
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  return h1;
+} 
