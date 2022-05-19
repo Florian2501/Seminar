ff2c4065c7b602eb6f1190fbff3bb70c1a9fa76f
Florian2501 <florischierz@gmail.com>
2022-05-19 16:39:06 +0200

    FÜge Graphen in PPP

---
+++ CProgrammierung/Implementation/Plots/FPP/plot.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+cur_path = os.path.dirname(__file__)
+
+new_path = os.path.relpath('graph_0.01_8.txt', cur_path)
+
+data = np.loadtxt(new_path)
+
+plt.plot(data[:,0], data[:,1], label='Standard BF')
+plt.plot(data[:,0], data[:,2], label='Floom-Filter')
+plt.plot(data[:,0], data[:,3], label='Big BF')
+
+plt.show()
